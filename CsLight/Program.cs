5d2b28f67ed27aa94c8c6d604b0548d5205fc41d
Олександр Кузьо <kuzo@rozetka.com.ua>
2022-12-02 15:58:01 +0200

    first commit

---
+++ CsLight/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace CsLight
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.GetEncoding (1251);
+            
+            Console.WriteLine("Генератор заточки");
+
+
+            Random rand = new Random();
+            int i;
+            int c = 0;
+            int a;
+            
+            Console.Write("Введите уровень заточки от 1 до 12: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            // if (a <= 3)
+            // {
+            //     Console.WriteLine("Поехали!");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Хорошая попытка! Но все же введите значение от 1 до 12");
+            // }
+            
+            while (true)
+            {
+                i = rand.Next(0, 2);
+                Console.WriteLine(i);
+                if (i == 1)
+                {
+                    c++;
+                    if ( c == a) 
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    c = 0;
+                }
+                
+            }
+            // Console.WriteLine("Для уровня заточки " + a + " нужно " + i + " миражей"); 
+        }
+    }
+}
